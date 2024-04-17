6a561b89acfd18a6ffb362e415c163f4348eb1b3
Dominik Bühler <mortelligaming@github.com>
2024-04-18 01:27:53 +0200

    added computed chainId to all chain-pages

--- src/pages/[...chainId]/parameters.vue
+++ src/pages/[...chainId]/parameters.vue
@@ -1,7 +1,18 @@
 <template>
+  <div v-if="chainId">
+  {{ chainId }}
   parameters
+  </div>
+  <not-found v-else />
 </template>
 
 <script lang="ts" setup>
-  //
+import { Ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import NotFound from '@/components/404.vue'
+
+const route = useRoute()
+const chainId: Ref<string> = computed(() => {
+  return (route.params as { chainId: string }).chainId || '' as string;
+})
 </script>
