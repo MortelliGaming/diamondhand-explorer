1267eceb32eb7b5885a79219fd72e72c6f611a25
Dominik Bühler <mortelligaming@github.com>
2024-04-28 21:06:38 +0200

    simplified proposal header

--- src/components/governance/ProposalHeaderRow.vue
+++ src/components/governance/ProposalHeaderRow.vue
@@ -1,63 +1,52 @@
 <template>
-    <v-row class="pa-2" role="button" @click="emit('click')">
-        <v-col cols="1"  class="d-flex justify-center align-center">
-            <v-chip class="justify-center">{{ props.proposal?.proposalId }}</v-chip>
-        </v-col>
-        <v-col cols="6" class="d-flex flex-grow-1 justify-center align-center" v-if="props.proposal">
-            <v-row class="pl-2 pr-2 text-caption" style="width: 100%;">
-                <v-col cols="12" class="text-subtitle-2">
-                    <b>{{ props.proposal?.content?.typeUrl?.split('.')[props.proposal?.content?.typeUrl?.split('.').length -1] }}</b>
-                </v-col>
-                <div v-if="props.proposal?.status != ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD">
-                    <v-col cols="12" class="pt-0 pb-0">
-                        <div class="pr-2">
-                            <b>{{ t('proposal.votingStartTime') }}:</b>
-                        </div>
-                        <div class="d-flex flex-row">
-                            <div class="pr-2">
-                                {{ moment(getTimeFromTimestamp(props.proposal.votingStartTime)).format('DD.MM.YY HH:mm:ss') }}
-                            </div>
-                            <div v-if="$vuetify.display.smAndUp" class="d-flex flex-row">
-                                <human-readable-time :time="props.proposal.votingStartTime" />
-                            </div>
-                        </div>
+    <v-container>
+        <v-row class="pa-2" role="button" @click="emit('click')">
+            <v-col cols="1"  class="d-flex justify-center align-center">
+                <v-chip class="justify-center">{{ props.proposal?.proposalId }}</v-chip>
+            </v-col>
+            <v-col cols="6" class="d-flex flex-grow-1 justify-center align-center" v-if="props.proposal">
+                <v-row class="pl-2 pr-2 text-caption" style="width: 100%;">
+                    <v-col cols="12" class="text-subtitle-2">
+                        <b>{{ props.proposal?.content?.typeUrl?.split('.')[props.proposal?.content?.typeUrl?.split('.').length -1] }}</b>
                     </v-col>
-                    <v-col cols="12" class="pt-0">
-                        <div class="pr-2">
-                            <b>{{ t('proposal.votingEndTime') }}:</b>
-                        </div>
-                        <div class="d-flex flex-row">
+                    <div v-if="props.proposal?.status != ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD">
+                        <v-col cols="12" class="pt-0 flex-row d-flex">
                             <div class="pr-2">
-                                {{ moment(getTimeFromTimestamp(props.proposal.votingEndTime)).format('DD.MM.YY HH:mm:ss') }}
+                                <b>{{ t('proposal.votingEndTime') }}:</b>
                             </div>
-                            <div v-if="$vuetify.display.smAndUp" class="d-flex flex-row">
-                                <human-readable-time :time="props.proposal.votingEndTime" />
+                            <div class="d-flex flex-row">
+                                <div class="pr-2">
+                                    {{ moment(getTimeFromTimestamp(props.proposal.votingEndTime)).format('DD.MM.YY HH:mm:ss') }}
+                                </div>
+                                <div v-if="$vuetify.display.smAndUp" class="d-flex flex-row">
+                                    <human-readable-time :time="props.proposal.votingEndTime" />
+                                </div>
                             </div>
-                        </div>
-                    </v-col>
-                </div>
-                <div v-else>
-                    <v-col class="d-flex  flex-column flex-sm-row">
-                        <div class="pr-2">
-                            <b>{{ t('proposal.depositEndTime') }}:</b>
-                        </div>
-                        <div class="d-flex flex-row">
+                        </v-col>
+                    </div>
+                    <div v-else>
+                        <v-col class="d-flex  flex-column flex-sm-row">
                             <div class="pr-2">
-                                {{ moment(getTimeFromTimestamp(props.proposal.depositEndTime)).format('DD.MM.YY HH:mm:ss') }}
+                                <b>{{ t('proposal.depositEndTime') }}:</b>
                             </div>
-                            <div v-if="$vuetify.display.smAndUp" class="d-flex flex-row">
-                                <human-readable-time :time="props.proposal.depositEndTime" />
+                            <div class="d-flex flex-row">
+                                <div class="pr-2">
+                                    {{ moment(getTimeFromTimestamp(props.proposal.depositEndTime)).format('DD.MM.YY HH:mm:ss') }}
+                                </div>
+                                <div v-if="$vuetify.display.smAndUp" class="d-flex flex-row">
+                                    <human-readable-time :time="props.proposal.depositEndTime" />
+                                </div>
                             </div>
-                        </div>
-                    </v-col>
-                </div>
-            </v-row>
-        </v-col>
-        <v-col cols="5" class="justify-end d-flex align-center">
-            <slot name="append"></slot>
-        </v-col>
-    </v-row>
-    <v-divider />
+                        </v-col>
+                    </div>
+                </v-row>
+            </v-col>
+            <v-col cols="5" class="justify-end d-flex align-center">
+                <slot name="append"></slot>
+            </v-col>
+        </v-row>
+        <v-divider />
+    </v-container>
 </template>
 
 <script lang="ts" setup>
