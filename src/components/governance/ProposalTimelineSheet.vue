600d66944341c28147706611fcb9ae747d1ed821
Dominik Bühler <mortelligaming@github.com>
2024-04-28 19:42:33 +0200

    better layout of proposal times

--- src/components/governance/ProposalTimelineSheet.vue
+++ src/components/governance/ProposalTimelineSheet.vue
@@ -3,25 +3,25 @@
         <v-container class="pt-0">
             <div class="text-h6">{{ t('proposal.dates') }}</div>
             <v-row class="d-flex justify-center pt-2">
-                <v-col class="text-center">
-                    <v-sheet elevation="24" color="grey-darken-3" class="pa-1">
+                <v-col cols="6" sm="3" class="text-center">
+                    <v-sheet elevation="6" color="grey-darken-3" class="pa-1">
                         <time-formatter :time="props.proposal?.submitTime" :title="t('proposal.submitTime')" />
                     </v-sheet>
                 </v-col>
-                <v-col class="text-center">
-                    <v-sheet elevation="24" color="grey-darken-3" class="pa-1">
+                <v-col cols="6" sm="3" class="text-center">
+                    <v-sheet elevation="6" color="grey-darken-3" class="pa-1">
                         <time-formatter :time="props.proposal?.depositEndTime" :title="t('proposal.depositEndTime')" />
                     </v-sheet>
                     
                 </v-col>
-                <v-col class="text-center">
-                    <v-sheet elevation="24" color="grey-darken-3" class="pa-1">
+                <v-col cols="6" sm="3" class="text-center">
+                    <v-sheet elevation="6" color="grey-darken-3" class="pa-1">
                         <time-formatter :time="props.proposal?.votingStartTime" :title="t('proposal.votingStartTime')" />
                     </v-sheet>
                     
                 </v-col>
-                <v-col class="text-center">
-                    <v-sheet elevation="24" color="grey-darken-3" class="pa-1">
+                <v-col cols="6" sm="3" class="text-center">
+                    <v-sheet elevation="6" color="grey-darken-3" class="pa-1">
                         <time-formatter :time="props.proposal?.votingEndTime" :title="t('proposal.votingEndTime')" />
                     </v-sheet>
                     
