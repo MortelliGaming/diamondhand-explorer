30996eb29d52ec735f9f11e16b9b9a84ba6fc5b0
Dominik Bühler <mortelligaming@github.com>
2024-05-20 11:16:10 +0200

    better display of proposal headers

--- src/components/governance/ProposalHeaderRow.vue
+++ src/components/governance/ProposalHeaderRow.vue
@@ -1,50 +1,55 @@
 <template>
-    <v-container class="pa-0">
-        <v-row class="pa-2" role="button" @click="emit('click')">
-            <v-col cols="1" class="d-flex justify-center align-center pb-2">
-                <v-chip class="justify-center">{{ props.proposal?.id }}</v-chip>
-            </v-col>
-            <v-col cols="6" class="d-flex flex-grow-1 justify-center align-center" v-if="props.proposal">
-                <v-row class="pl-2 pr-2 text-caption" style="width: 100%;" no-gutters>
-                    <v-col cols="12" class="text-subtitle-2" v-for="(msg,i) in props.proposal?.messages" :key="'msg_'+i">
-                        <b>{{ msg.typeUrl?.split('.')[msg.typeUrl?.split('.').length -1] }}</b>
-                    </v-col>
-                    <div v-if="props.proposal?.status != ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD">
-                        <v-col cols="12" class="pa-0 flex-row d-flex">
-                            <div class="pr-2">
-                                <b>{{ t('proposal.votingEndTime') }}:</b>
-                            </div>
-                            <div class="d-flex flex-row">
-                                <div class="pr-2" v-if="props.proposal.votingEndTime">
-                                    {{ moment(getTimeFromTimestamp(props.proposal.votingEndTime)).format('DD.MM.YY HH:mm:ss') }}
+    <v-container class="pa-0" @click="emit('click')">
+        <v-row no-gutters cols="12">
+                <v-col cols="12" class="text-subtitle-2" v-for="(msg,i) in props.proposal?.messages" :key="'msg_'+i">
+                    <b>{{ msg.typeUrl?.split('.')[msg.typeUrl?.split('.').length -1] }}</b>
+                </v-col>
+                <v-col cols="12" v-if="proposal?.messages?.length == 0" class="text-subtitle-2">
+                    <b>{{ proposal?.title }}</b>
+                </v-col>
+            </v-row>
+            <v-row no-gutters class="pt-2 pb-2">
+                <v-col cols="1" class="d-flex align-center">
+                    <v-chip class="justify-center">{{ props.proposal?.id }}</v-chip>
+                </v-col>
+                <v-col cols="6" class="pl-2 d-flex flex-grow-1 justify-center align-center" v-if="props.proposal">
+                    <v-row class="text-caption" no-gutters>
+                        <div v-if="props.proposal?.status != ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD">
+                            <v-col cols="12" class="pa-0 flex-row d-flex">
+                                <div class="pr-2 d-flex align-center">
+                                    <b>{{ t('proposal.votingEndTime') }}:</b>
                                 </div>
-                                <div v-if="$vuetify.display.smAndUp" class="d-flex flex-row">
-                                    <human-readable-time :time="props.proposal.votingEndTime" />
+                                <div class="d-flex flex-row">
+                                    <div class="pr-2" v-if="props.proposal.votingEndTime">
+                                        {{ moment(getTimeFromTimestamp(props.proposal.votingEndTime)).format('DD.MM.YY HH:mm:ss') }}
+                                    </div>
+                                    <div v-if="$vuetify.display.smAndUp" class="d-flex flex-row">
+                                        <human-readable-time :time="props.proposal.votingEndTime" />
+                                    </div>
                                 </div>
-                            </div>
-                        </v-col>
-                    </div>
-                    <div v-else>
-                        <v-col class="d-flex  flex-column flex-sm-row">
-                            <div class="pr-2">
-                                <b>{{ t('proposal.depositEndTime') }}:</b>
-                            </div>
-                            <div class="d-flex flex-row">
-                                <div class="pr-2" v-if="props.proposal.depositEndTime">
-                                    {{ moment(getTimeFromTimestamp(props.proposal.depositEndTime)).format('DD.MM.YY HH:mm:ss') }}
+                            </v-col>
+                        </div>
+                        <div v-else>
+                            <v-col class="d-flex  flex-column flex-sm-row">
+                                <div class="pr-2">
+                                    <b>{{ t('proposal.depositEndTime') }}:</b>
                                 </div>
-                                <div v-if="$vuetify.display.smAndUp" class="d-flex flex-row">
-                                    <human-readable-time :time="props.proposal.depositEndTime" />
+                                <div class="d-flex flex-row">
+                                    <div class="pr-2" v-if="props.proposal.depositEndTime">
+                                        {{ moment(getTimeFromTimestamp(props.proposal.depositEndTime)).format('DD.MM.YY HH:mm:ss') }}
+                                    </div>
+                                    <div v-if="$vuetify.display.smAndUp" class="d-flex flex-row">
+                                        <human-readable-time :time="props.proposal.depositEndTime" />
+                                    </div>
                                 </div>
-                            </div>
-                        </v-col>
-                    </div>
-                </v-row>
-            </v-col>
-            <v-col cols="5" class="justify-end d-flex align-center">
-                <slot name="append"></slot>
-            </v-col>
-        </v-row>
+                            </v-col>
+                        </div>
+                    </v-row>
+                </v-col>
+                <v-col cols="5" class="justify-end d-flex align-center">
+                    <slot name="append"></slot>
+                </v-col>
+            </v-row>
         <v-divider />
     </v-container>
 </template>
