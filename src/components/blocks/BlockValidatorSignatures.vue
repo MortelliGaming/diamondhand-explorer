38b151db67acc0335ce0427e54f875f2f2afbc29
Dominik Bühler <mortelligaming@github.com>
2024-05-23 12:36:53 +0200

    added uptime

---
+++ src/components/blocks/BlockValidatorSignatures.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-col
+    cols="12" sm="3" class="pa-1">
+    <v-card>
+      <v-card-text class="text-caption">
+        <v-row>
+          <v-col cols="12" class="pa-1">
+            <v-row no-gutters>
+              <v-col
+                class="d-flex align-center">
+                  <validator-header :validator="validator" />
+                  <div class="pl-1">#{{ props.rank}}</div>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="12" class="pa-1">
+            <v-row cols="100" no-gutters>
+              <v-tooltip
+                v-for="(block, i) in latest100Blocks || []"
+                :key="(block?.header.appHash.toString() || '' + i)"
+                :text="block?.header.height.toString()">
+                <template v-slot:activator="{ props }">
+                  <v-col
+                    v-bind="props"
+                    cols="1"
+                    class="pa-0 signature">
+                    <div
+                      v-if="block"
+                      :class="'signature ' + (block.lastCommit!.signatures.find(s => s.validatorAddress?.toString() || '' == validatorConsensusAddress) ? 'green' : 'red')">
+                    </div>
+                    <div
+                      v-else
+                      :class="'signature gray'">
+                    </div>
+                  </v-col>
+                </template>
+              </v-tooltip>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-col>
+</template>
+
+<script lang="ts" setup>
+// External
+import { storeToRefs } from 'pinia'
+
+// Store
+import { useAppStore } from '@/store/app'
+import { useBlockchainStore } from '@/store/blockchain';
+
+// Components
+import ValidatorHeader from '@/components/ValidatorHeader.vue';
+import { computed, PropType, ref } from 'vue';
+import { ExtendedValidator } from '../../store/validators';
+
+import { fromHex } from '@cosmjs/encoding'
+
+const props = defineProps({
+  validator: {
+    type: Object as PropType<ExtendedValidator>,
+    required: true,
+  },
+  rank: String
+})
+
+const { chainIdFromRoute } = storeToRefs(useAppStore())
+const { latestBlocks } = storeToRefs(useBlockchainStore())
+
+const latestChainBlocks= computed(() => {
+  return latestBlocks.value[chainIdFromRoute.value || '']
+})
+
+const latest100Blocks = computed(() => {
+  return latestChainBlocks.value?.filter(b => b.lastCommit != null).splice(0,100)
+})
+const validatorConsensusAddress = ref(fromHex(props.validator.consensusHexAddress.replace('0x', ''))?.toString())
+
+</script>
+<style lang="scss" scoped>
+  .list-move, /* apply transition to moving elements */
+  .list-enter-active,
+  .list-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  .list-enter-from,
+  .list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+  }
+
+  /* ensure leaving items are taken out of layout flow so that moving
+    animations can be calculated correctly. */
+  .list-leave-active {
+    position: absolute;
+  }
+
+  .signature {
+    height: 15px !important;
+    width: 3px;
+    max-width: 3px;
+    margin-right: 2px;
+    margin-top: 2px;
+    &.green {
+      background-color: green;
+    }
+    &.red {
+      background-color: red;
+    }
+    &.gray {
+      background-color: gray;
+    }
+  }
+</style>
--- src/pages/[chainId]/uptime.vue
+++ src/pages/[chainId]/uptime.vue
@@ -1,81 +1,33 @@
 <template>
   <chain-content>
-    <base-sheet :title="t('uptime.uptime')"  style="max-height: 80vh; overflow-y: scroll; width: 100%;">
-      <v-row  style="overflow-y: scroll; overflow-x:hidden;">
-        <transition-group name="list">
-          <v-col cols="6" sm="4" md="3" v-for="block in latestChainBlocks" :key="block.header.appHash.toString()" >
-            <v-sheet
-              @click="$router.push('./block/'+block.header.height)"
-              role="button"
-              color="grey-darken-3" 
-              rounded 
-              elevation="12" 
-              class="pa-2 fill-height" 
-              style="min-height: 96px;">
-                <v-row no-gutters class="d-flex flex-row">
-                  <v-col cols="12"  class="d-flex flex-row">
-                    <div class="d-flex justify-center" >
-                      <b>#{{  block.header.height  }}</b>
-                    </div>
-                    <div class="text-right d-flex flex-grow-1 justify-end">
-                      <v-chip size="x-small" color="success">
-                        {{ block.txs.length }}
-                      </v-chip>
-                    </div>
-                  </v-col>
-                  <v-col cols="12" class="d-flex align-center justify-center" style="min-height:40px;">
-                    <v-avatar v-if="getValidator(block.header.proposerAddress)" class="mr-2" size="x-small">
-                          <v-img 
-                              v-if="keybaseAvatars[getValidator(block.header.proposerAddress)!.description.identity]"
-                              :src="keybaseAvatars[getValidator(block.header.proposerAddress)!.description.identity]" />
-                          <v-icon v-else icon="mdi-account" />
-                      </v-avatar>
-                  </v-col>
-                  <v-col cols="12" class="text-center">
-                      <b>{{ getValidator(block.header.proposerAddress)?.description.moniker  }}</b>
-                  </v-col>
-                  <v-col cols="12" class="text-center">{{  moment(block.header.time.toISOString()).format('DD.MM.YY HH:mm:ss')  }}</v-col>
-                </v-row>
-            </v-sheet>
-          </v-col>
-        </transition-group>
-      </v-row>
-      </base-sheet>
+    <v-row class="pl-2 pr-2">
+      <block-validator-signatures
+        v-for="(val, i) in validators[chainIdFromRoute]?.filter((v) => v.status == BondStatus.BOND_STATUS_BONDED).sort((a,b) => Number(b.tokens) - Number(a.tokens))"
+        :key="val.operatorAddress"
+        :rank="(i + 1).toString()"
+        :validator="getValidatorInfo(chainIdFromRoute, val)" />
+    </v-row>
   </chain-content>
 </template>
 
 <script lang="ts" setup>
 // External
 import { storeToRefs } from 'pinia'
-import { useI18n } from 'vue-i18n';
-import { Buffer } from 'buffer';
 
 // Store
 import { useAppStore } from '@/store/app'
-import { useBlockchainStore } from '@/store/blockchain';
 import { useValidatorsStore } from '@/store/validators';
 
 // Components
 import ChainContent from '@/components/ChainContent.vue'
-import BaseSheet from '@/components/BaseSheet.vue';
-import { computed } from 'vue';
-import moment from 'moment';
+import BlockValidatorSignatures from '@/components/blocks/BlockValidatorSignatures.vue';
 
-const { t } = useI18n()
+import { BondStatus } from '@/lib/proto/cosmos/staking/v1beta1/staking';
 
 const { chainIdFromRoute } = storeToRefs(useAppStore())
-const { latestBlocks } = storeToRefs(useBlockchainStore())
-const { validators, keybaseAvatars } = storeToRefs(useValidatorsStore())
+const { validators } = storeToRefs(useValidatorsStore())
 const { getValidatorInfo, loadCosmosValidators } = useValidatorsStore()
 
-const latestChainBlocks= computed(() => {
-  return latestBlocks.value[chainIdFromRoute.value || '']
-})
-
-function getValidator(proposerAddressBytes: Uint8Array) {
-  const hexAddress = '0x' + Buffer.from(proposerAddressBytes).toString('hex')
-  return validators.value[chainIdFromRoute.value || '']?.map(v => getValidatorInfo(chainIdFromRoute.value || '', v))?.find(v => v.consensusHexAddress.toLowerCase() == hexAddress.toLowerCase())
-}
 if(!validators.value[chainIdFromRoute.value || '']) {
   await loadCosmosValidators(chainIdFromRoute.value || '')
 }
@@ -98,5 +50,19 @@ if(!validators.value[chainIdFromRoute.value || '']) {
     animations can be calculated correctly. */
   .list-leave-active {
     position: absolute;
+  }
+
+  .signature {
+    height: 15px !important;
+    width: 3px;
+    max-width: 3px;
+    margin-right: 2px;
+    margin-top: 2px;
+    &.green {
+      background-color: green;
+    }
+    &.red {
+      background-color: red;
+    }
   }
 </style>
